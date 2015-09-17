ee1c86555cec4572aacd4537173eb4003fc0131d
virtushda <virtushda@gmail.com>
2015-09-17 13:51:52 -0400

    more easygui

--- superspritesheetgen.py
+++ superspritesheetgen.py
@@ -3,15 +3,16 @@ from PIL import Image
 import easygui
 import math
 
-opath = easygui.fileopenbox()
-fsize = easygui.integerbox(msg='Frame Size?', lowerbound=4, upperbound=2048)
-fcount = easygui.integerbox(msg='How many frames?', lowerbound=1, upperbound=1024)
-fcountsqr = math.sqrt(fcount)
-colcount = (fcountsqr)
+opath = easygui.fileopenbox(title='Pick an image sequence!')
+tsize = easygui.integerbox(msg='Texture Size?', lowerbound=16, upperbound=4094)
+#fcount = easygui.integerbox(msg='How many frames?', lowerbound=1, upperbound=1024)
+#fcountsqr = math.sqrt(fcount)
+#colcount = math.ceil(fcountsqr)
+#print (colcount)
     
 img = Image.open(opath)
     
-img = img.resize((fsize,fsize))
+img = img.resize((tsize,tsize))
 img.c = img.copy()
 print (img.c)
 
